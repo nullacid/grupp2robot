c237aedddec1a7b6609b8a38e8acf3eada95f7d6
S̮̠̝̗̻̩͎͔͚̲̞͕̦͉͇̟̞̩͈͞͡h҉͇̣̹̠̬̪̦̫̣̞͈̫̘͈́ͅa̷̵̡̡̫̣̲d̩̖̭͓̼͔̯̳̜͖͖̭̫̲͝o̢҉̷̙̥͓̺̻̦͖̯͉ẃ͉͕̜̱̦͔͕̮̹̜͓̮́́͢Outcast________________________1337 <pettu298@student.liu.se>
2015-11-12 16:54:30 +0000

    Began adding comm functionality to mimer

--- src/system/mimer.c
+++ src/system/mimer.c
@@ -8,6 +8,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <math.h>
+#include "lib\usart.h"
 
 uint8_t cm_values[120] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 96, 90, 85, 79, 75, 71, 67, 64, 61, 58, 55, 53, 51, 49, 47, 45, 43, 42, 40, 39, 38, 37, 35, 34, 33, 32, 31, 31, 30, 29, 28, 28, 27, 26, 26, 25, 24, 24, 23, 23, 22, 22, 21, 21, 20, 20, 20, 19, 19, 19, 18, 18, 18, 17, 17, 17, 16, 16, 16, 16, 15, 15, 15, 15, 14, 14, 14, 14, 14, 13, 13, 13, 13, 13, 13, 12, 12, 12, 12, 12, 12, 11, 11, 11, 11, 11, 11, 11, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 9, 9, 9, 9};
 
@@ -49,6 +50,61 @@ int16_t adc_to_cm(int16_t value){
 	return cm_values[value];
 }
 
+/* Transits 2 dummy bytes (LIDAR size is 2 bytes)*/
+void transmitLidar(){
+	transmitByte_up(0x05);
+	transmitByte_up(0x08);
+}
+
+void transmitIRRF(){
+	transmitByte_up(0x5d);
+}
+
+void processCommand(unsigned char data){
+	if(data == 0x08){
+		transmitLidar();
+	}
+	else if(data == 0x09){
+		//IRRF
+	}
+	else if(data == 0x0A){
+		//IRRB
+	}
+	else if(data == 0x0B){
+		//IRLF
+	}
+	else if(data == 0x0C){
+		//IRLB
+	}
+	else if(data == 0x0D){
+		//GYRO
+	}
+	else if(data == 0x0F){
+		//LIDAR TOKEN
+	}
+	else if(data == 0x10){
+		//PARALLEL RIGHT
+	}
+	else if(data == 0x11){
+		//PARALLEL LEFT
+	}
+	else if(data == 0x12){
+		//GYRO TOKEN
+	}
+	else if(data == 0x13){
+		//IRRF token
+	}
+	else if(data == 0x14){
+		//IRRB token
+	}
+	else if(data == 0x15){
+		//IRLF token
+	}
+	else if(data == 0x16){
+		//IRLB token
+	}
+}
+
 int main()
 {
 	uint8_t i = 0;
@@ -58,6 +114,9 @@ int main()
 	
 	// initialize adc
 	adc_init();
+	
+	//Baud rate is 10
+	init_USART_up(10);
 	
 	_delay_ms(50);
 	
@@ -76,6 +135,15 @@ int main()
 		}
 		i++;
 		*/
+		
+		
+		if(checkUSARTflag_up()){
+			data = receiveByte_up();
+			data &= 0x6F	//Maska ut kommandot
+			processCommand(data);
+		}
+		
+		
 		PORTD = adc_to_cm(adc_read(0));
 	
 		_delay_ms(50);
