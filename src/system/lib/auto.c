556d5c568f176e0d6c5e88adbf35ae91e05af510
nullacid <mikaelangman@hotmail.com>
2015-11-17 10:03:37 +0100

    Added auto

---
+++ src/system/lib/auto.c
@@ -0,0 +1,13 @@
+#import "auto.h"
+
+
+//------------------Constants----------------
+
+uint8_t IR_OFFSET = 7;
+uint8_t OK_DISTANCE_TO_WALL = 20;
+uint8_t LIDAR_OFFSET;
+uint8_t FLUFF = 1;
+uint8_t 90DEG;
+
+
+//-------------------------------------------
