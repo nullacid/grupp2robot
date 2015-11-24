e548220d26de92199fd4d25fbef6668879518a43
michael <michael.sorsater@gmail.com>
2015-11-24 16:32:09 +0100

    now better function for ir-values

--- src/system/temp/ir_lista.py.py
+++ src/system/temp/ir_lista.py.py
@@ -1,14 +1,12 @@
 from math import *
 
-def test_value(x):
-    print(26.047*pow(x,-1.252));
-
 def test_value_bin(x):
+	#old function 26.047*pow(x_r,-1.252)
     if x < 18:
         return 0
     x_r = x/256*5;
-    print("x= ", x, " y= ", 26.047*pow(x_r,-1.252))
-    return 26.047*pow(x_r,-1.252)
+    print("x= ", x, " y= ", 27.166*pow(x_r,-1.225))
+    return 27.166*pow(x_r,-1.225)
 
 def display(higher):
     res = []
@@ -18,5 +16,5 @@ def display(higher):
     print(res)
     print(len(res))
 
-display(120)
+display(200)
         
