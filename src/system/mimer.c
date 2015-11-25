a04ec5d0efb80650f480895d417281436e861a3e
Michael Sörsäter <michael.sorsater@gmail.com>
2015-11-25 08:42:37 +0100

    updated ir-values

--- src/system/mimer.c
+++ src/system/mimer.c
@@ -4,33 +4,35 @@
  * Created: 11/9/2015 2:49:39 PM
  *  Author: micso554
  */
-
-#define F_CPU 20000000UL
+#ifndef F_CPU
+#define F_CPU 20000000
+#endif
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <math.h>
 #include "lib\usart.h"
+#include <avr/interrupt.h>
 
-uint8_t cm_values[120] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 96, 90, 85, 79, 75, 71, 67, 64, 61, 58, 55, 53, 51, 49, 47, 45, 43, 42, 40, 39, 38, 37, 35, 34, 33, 32, 31, 31, 30, 29, 28, 28, 27, 26, 26, 25, 24, 24, 23, 23, 22, 22, 21, 21, 20, 20, 20, 19, 19, 19, 18, 18, 18, 17, 17, 17, 16, 16, 16, 16, 15, 15, 15, 15, 14, 14, 14, 14, 14, 13, 13, 13, 13, 13, 13, 12, 12, 12, 12, 12, 12, 11, 11, 11, 11, 11, 11, 11, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 9, 9, 9, 9};
+int ir_values_length = 200;
+
+uint8_t cm_values[ir_values_length] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 98, 91, 86, 81, 76, 72, 69, 65, 62, 59, 57, 55, 52, 50, 48, 47, 45, 43, 42, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 29, 28, 27, 27, 26, 25, 25, 24, 24, 23, 23, 22, 22, 21, 21, 21, 20, 20, 20, 19, 19, 19, 18, 18, 18, 17, 17, 17, 16, 16, 16, 16, 15, 15, 15, 15, 15, 14, 14, 14, 14, 14, 13, 13, 13, 13, 13, 13, 12, 12, 12, 12, 12, 12, 12, 11, 11, 11, 11, 11, 11, 11, 11, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
 	
 uint8_t IRLF = 0x01;
 uint8_t IRLB = 0x02;
 uint8_t IRRF = 0x03;
 uint8_t IRRB = 0x04;
 
-uint8_t IRLFT = 0x0a;
-uint8_t IRLBT = 0x0b;
-uint8_t IRRFT = 0x0c;
-uint8_t IRRBT = 0x0d;
-
-uint8_t parallelL = 0xa0;
-uint8_t parallelR = 0xb0;
-
-uint8_t lidarT = 0x45;
+uint8_t angular_rate = 0x00;
+int angular_sum = 0;
 
 uint8_t LIDAR_H = 0xa0;
 uint8_t LIDAR_L = 0xa1;
+uint8_t zero = 0;
+
+uint8_t gyro1 = 0xb0;
+uint8_t gyro2 = 0xb1;
+//,gyro3,gyro4;
 
 // current PWM-value
 int current = 0;
@@ -39,7 +41,11 @@ void timer1_init(){
 	// reset registers and set prescaler to 1024
 	TCCR1A = 0;
 	TCCR1B = 0;
-	TCCR1B |= (1<<CS10)|(0<<CS11)|(1<<CS12);
+	
+	//TCCR1B |= (1<<CS10)|(0<<CS11)|(1<<CS12);
+	
+	// prescaler 8 i
+	TCCR1B |= (0<<CS10)|(1<<CS11)|(0<<CS12);
 	
 	// reset timer
 	TCNT1 = 0;
@@ -54,6 +60,34 @@ void adc_init()
 	// ADC Enable and prescaler of 128
 	// 20 000 000/128 = 156 250 Hz (50 kHz < 156 kHz < 200 kHz)
 	ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
+}
+
+
+
+void set_ss(int mode){
+	PORTB &= (mode<<PORTB4);
+}
+
+
+void SPI_init() //SPI initialization
+{
+	
+	DDRB=(1<<PCINT15)|(1<<PCINT13)|(1<<PORTB4); // set SCK, MOSI and SS as output
+	SPCR=(1<<SPE)|(1<<MSTR)|(1<<SPR0);//|(1<<CPHA); // Enable SPI | set as master | SCK freq = mclk/8
+	
+	set_ss(1);
+}
+
+unsigned char spi_tranceiver (uint8_t data)
+{
+	// Load data into the buffer
+	SPDR = data;
+	
+	//Wait until transmission complete
+	while(!(SPSR & (1<<SPIF) ));
+	
+	// Return received data
+	return(SPDR);
 }
 
 // read adc value
@@ -78,100 +112,14 @@ int16_t adc_read(int16_t ch)
 }
 
 int16_t adc_to_cm(int16_t value){
-	if (value > 120)
+	if (value > ir_values_length)
 		return 1;
 	return cm_values[value];
 }
 
-/* Calculates the token value given pointers to the stored values. */
-void calcTokenIR(uint8_t *IRxx, uint8_t *IRxxT){
-	if(1 < *IRxx && *IRxx <= 32){
-		*IRxxT = 1;
-	}
-	else if(32 < *IRxx && *IRxx < 90){
-		*IRxxT = 2;
-	}
-	else{
-		*IRxxT = 0;
-	}
-}
-
-/* Calculates the token values for the IR sensors. A 1 represents a value between 1 and 32 (a wall within 1 square), 2 represents a value between 32 and 89 ( a wall within 2 squares). */
-void calcTokensIR(){	
-	calcTokenIR(&IRLB, &IRLBT);
-	calcTokenIR(&IRLF, &IRLFT);
-	calcTokenIR(&IRRB, &IRRBT);
-	calcTokenIR(&IRRF, &IRRFT);
-}
-
-void calcLidarT(){
-	uint16_t lidarValue = LIDAR_H*256 + LIDAR_L;
-	lidarT = lidarValue / 40;
-}
-
-/* Calculates the token values for parallelity by taking the quotient of the front and back sensors. 0 represents parallelity. 
-   1 and 2 represents a positive offset in the front sensor (front sensor is further away from the wall, turn right to fix this).
-   3 and 4 represents a negative offset in the front sensor (front sensor is closer to the wall, turn left to fix this).
-   The lower value represents a smaller offset. 
-   Will return 0xFF if there is not a wall within one square. */
-void calcParallel(){
-	//Calculates parallelity for the right sensors.
-	if(IRRFT == 1 && IRRBT == 1){
-		double floatingIRRF = IRRF;
-		double floatingIRRB = IRRB;
-		double quotient = 1 - (floatingIRRF/floatingIRRB);
-		uint8_t offset = 0;
-		//Negative quotient means that IRRF > IRRB ((1 - >1) < 0). Sets offset to 2 so that the token values will be set to 3 and 4 instead of 1 and 2.
-		if(quotient > 0){
-			offset = 2;
-			quotient = fabs(quotient); //Takes the absolute value of quotient to make calculations easier.
-		}
-		
-		if(quotient < 0.1){
-			parallelR = 0x00;
-		}
-		else if(quotient < 0.2){
-			parallelR = offset + 0x01;
-		}
-		else if(quotient < 0.4){
-			parallelR = offset + 0x02;
-		}
-	}
-	else{
-		parallelR = 0xFF;
-	}
-	
-	//Calculates parallelity for the left sensors.
-	if(IRLFT == 1 && IRLBT == 1){
-		double floatingIRLF = IRLF;
-		double floatingIRLB = IRLB;
-		double quotient = 1 - (floatingIRLF/floatingIRLB);
-		uint8_t offset = 0;
-		if(quotient < 0){
-			offset = 2;
-			quotient = fabs(quotient);
-		}
-		if(quotient < 0.1){
-			parallelL = 0x00;
-		}
-		else if(quotient < 0.2){
-			parallelL = offset + 0x01;
-		}
-		else if(quotient < 0.4){
-			parallelL = offset + 0x02;
-		}
-	}
-	else{
-		parallelL = 0xFF;
-	}
-}
-
-
-
-
 /* These functions should get the internally stored values and transmit them, the functions sending two bytes will probably need to do some shifting. */
 
-/* Transmits data from LIDAR. 2 bytes. */
+/* Transmits data from LIDAR. 2 bytes. MOST SIGNIFICANT BYTE NEED TO BE SENT FIRST. */
 void transmitLidar(){
 	transmitByte_up(LIDAR_H);
 	transmitByte_up(LIDAR_L);
@@ -194,20 +142,22 @@ void transmitIRLB(){
 }
 /* Transmits data from the Gyro. 2 bytes. MOST SIGNIFICANT BYTE NEED TO BE SENT FIRST. */
 void transmitGyro(){
-	transmitByte_up(0x21);
-	transmitByte_up(0x22);
+//	transmitByte_up(gyro4);
+//	transmitByte_up(gyro3);
+	transmitByte_up(gyro2);
+	transmitByte_up(gyro1);
 }
-/* Lidar Token is the result of integer division between the Lidar value and 40. */
+
 void transmitLidarT(){
-	transmitByte_up(lidarT);
+	transmitByte_up(0x99);
 }
 
 void transmitParallelR(){
-	transmitByte_up(parallelR);
+	transmitByte_up(0x5A);
 }
 
 void transmitParallelL(){
-	transmitByte_up(parallelL);
+	transmitByte_up(0x5B);
 }
 
 void transmitGyroT(){
@@ -215,19 +165,19 @@ void transmitGyroT(){
 }
 
 void transmitIRRFT(){
-	transmitByte_up(IRRFT);
+	transmitByte_up(0x5c);
 }
 
 void transmitIRRBT(){
-	transmitByte_up(IRRBT);
+	transmitByte_up(0x5d);
 }
 
 void transmitIRLFT(){
-	transmitByte_up(IRLFT);
+	transmitByte_up(0x5e);
 }
 
 void transmitIRLBT(){
-	transmitByte_up(IRLBT);
+	transmitByte_up(0x5f);
 }
 
 /* Transmits all sensor data. */
@@ -299,10 +249,14 @@ void processCommand(unsigned char data){
 	}
 }
 uint16_t read_lidar(){
-	uint16_t cm;
+	//uint16_t cm;
 	int prev = 0;
-	uint16_t timeOfMyLife;
+	//int timeOfMyLife;
 	int positive_edge_triggered = 0;
+	
+	double timeOfMyLife;
+	int low,high;
+	int cm;
 	
 	while(1){
 		
@@ -323,14 +277,15 @@ uint16_t read_lidar(){
 			if (prev == 1){
 				if (current == 0){
 					// read timer value
-					uint8_t low  = TCNT1;
-					uint8_t high = TCNT1H;
+					low  = TCNT1;
+					high = TCNT1H;
 			
 					// timeOfMyLife = high + low;
 					timeOfMyLife = (high << 8) |low;
 			
 					// conversion to cm with number magic
-					cm = timeOfMyLife *200/39;	
+					//cm = timeOfMyLife*200/39;	
+					cm = (int)timeOfMyLife/25;	
 					prev = 0;					 
 					positive_edge_triggered = 0;
 					
@@ -340,6 +295,142 @@ uint16_t read_lidar(){
 		}
 	}
 }
+void calibrate_gyro(){
+		unsigned char data;                 //Received data stored here
+		uint8_t res_adc1;
+		uint8_t res_adc2;
+		uint8_t angular_rate;
+
+		
+		// step1 put adc to the active mode if it wasn't
+		set_ss(0);
+		spi_tranceiver(0x94);
+		res_adc1 = spi_tranceiver(0x00);
+		spi_tranceiver(0x00);
+
+		set_ss(1);
+		_delay_us(115);
+		
+		// step 2 start conversion
+		set_ss(0);
+		spi_tranceiver(0x94);
+		res_adc1 = spi_tranceiver(0x00);
+		spi_tranceiver(0x00);
+
+		set_ss(1);
+		
+		_delay_us(115);
+
+		set_ss(0);
+		
+		// step 3 result obtaining
+		spi_tranceiver(0x80);
+		res_adc1 = spi_tranceiver(0x00);
+		res_adc2 = spi_tranceiver(0x00);
+
+		set_ss(1);
+		zero = ((res_adc1 & 0x0f) << 4) | ((res_adc2 & 0xf0) >> 4); // b5 = 0
+
+}
+int single_measure(){
+	uint8_t res_adc1;
+	uint8_t res_adc2;
+	set_ss(0);
+	spi_tranceiver(0x94);
+	res_adc1 = spi_tranceiver(0x00);
+	spi_tranceiver(0x00);
+	set_ss(1);
+	_delay_us(115);
+	set_ss(0);
+	
+	// step 3 result obtaining
+	spi_tranceiver(0x80);
+	res_adc1 = spi_tranceiver(0x00);
+	res_adc2 = spi_tranceiver(0x00);
+	set_ss(1);
+	
+	angular_rate = ((res_adc1 & 0x0f) << 4) | ((res_adc2 & 0xf0) >> 4); // b5 = 0
+	angular_sum = angular_sum + angular_rate-zero;
+	
+	_delay_ms(100);
+	
+	//gyro4 = (angular_sum & 0xf000) >> 24;
+	//gyro3 = (angular_sum & 0x0f00) >> 16;
+	return angular_sum;
+}
+void startMeasure(){
+	unsigned char data;                 //Received data stored here
+	uint8_t res_adc1;
+	uint8_t res_adc2;
+	uint8_t angular_rate;
+	
+	
+	// step1 put adc to the active mode if it wasn't
+	set_ss(0);
+	spi_tranceiver(0x94);
+	res_adc1 = spi_tranceiver(0x00);
+	spi_tranceiver(0x00);
+
+	set_ss(1);
+	_delay_us(115);
+	
+	// step 2 start conversion
+	set_ss(0);
+	spi_tranceiver(0x94);
+	res_adc1 = spi_tranceiver(0x00);
+	spi_tranceiver(0x00);
+
+	set_ss(1);
+	
+	_delay_us(115);
+
+	set_ss(0);
+	
+	// step 3 result obtaining
+	spi_tranceiver(0x80);
+	res_adc1 = spi_tranceiver(0x00);
+	res_adc2 = spi_tranceiver(0x00);
+
+	set_ss(1);
+	zero = ((res_adc1 & 0x0f) << 4) | ((res_adc2 & 0xf0) >> 4); // b5 = 0
+	
+	while (1){
+		
+		// step 2 start conversion
+		set_ss(0);
+		spi_tranceiver(0x94);
+		res_adc1 = spi_tranceiver(0x00);
+		spi_tranceiver(0x00);
+		
+		set_ss(1);
+		
+		_delay_us(115);
+		set_ss(0);
+		
+		// step 3 result obtaining
+		spi_tranceiver(0x80);
+		res_adc1 = spi_tranceiver(0x00);
+		res_adc2 = spi_tranceiver(0x00);
+		set_ss(1);
+		
+		angular_rate = ((res_adc1 & 0x0f) << 4) | ((res_adc2 & 0xf0) >> 4); // b5 = 0
+		angular_sum = angular_sum + angular_rate-zero;
+		
+		_delay_ms(100);
+		
+		//gyro4 = (angular_sum & 0xf000) >> 24;
+		//gyro3 = (angular_sum & 0x0f00) >> 16;
+		gyro2 = (angular_sum & 0x00f0) >> 8;
+		gyro1 = (angular_sum & 0x000f);
+		
+		//transmitByte_up(gyro4);
+		//transmitByte_up(gyro3);
+		//transmitByte_up(gyro2);
+		//transmitByte_up(gyro1);
+		
+	}
+}
+
 int main()
 {
 	uint8_t data;
@@ -348,6 +439,9 @@ int main()
 	timer1_init();
 	adc_init();
 	init_USART_up(10); 	//Baud rate is 10
+	//SPI_init();
+	//calibrate_gyro();
+	
 	
 	/*
 	// adc code.
@@ -357,8 +451,6 @@ int main()
 	*/
 	
 	// waiting for something good to happen
-	_delay_ms(500);
-	
 	while(1)
 	{		
 		/*
@@ -372,28 +464,34 @@ int main()
 		i++;
 		*/
 				
+				
+		PORTD |= 1<<PORTD5;
 		// usart
 		if(checkUSARTflag_up()){
+			PORTD |= 1<<PORTD4;
 			data = receiveByte_up();
 			data &= 0x3F;	//Maska ut kommandot
 			processCommand(data);
 		}
-		
+			
 		// lidar
 		cm = read_lidar();
 		LIDAR_L = cm & 0xff;
 		LIDAR_H = cm >> 8;		
 		
-		// adc
-		IRLF = adc_to_cm(adc_read(0));
-		IRLB = adc_to_cm(adc_read(1));
-		IRRF = adc_to_cm(adc_read(2));
-		IRRB = adc_to_cm(adc_read(3));
+		// adc	
 		
-		calcTokensIR();
-		calcParallel();
-		calcLidarT();
+		IRRF = adc_to_cm(adc_read(0));
+		IRLF = adc_to_cm(adc_read(1));
+		IRRB = adc_to_cm(adc_read(2));
+		IRLB = adc_to_cm(adc_read(3));
 		
-		PORTD = IRRB;
+		
+		
+		//int sum = single_measure();
+		//gyro2 = (sum & 0x00f0) >> 8;
+		//gyro1 = (sum & 0x000f);
+		//startMeasure();
+		
 	}
 }
