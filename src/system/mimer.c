f65128a206dc809ae96faf13971d231a019fb248
È̪̪̲͉͍̫̥r̛̰̟̱̱̼̙i͏̢̳̩̭͖̩͙k̵͈̲͇̕ ͕̮̦͈̯͡R̩͖̤͉̩̳ö̝̰̖̯͇n̷̬̟̝̦̙̗̙̰͎͟m̟͓a̸̹̼̥̯̭͓͢r̶̢͏̰͈͔͍̞k͙̱̳̠̰̼̩̀ <erik.ronmark.1114@gmail.com>
2015-12-11 16:40:06 +0100

    mimer update

--- src/system/mimer.c
+++ src/system/mimer.c
@@ -40,7 +40,7 @@ int8_t parallelR = 0x0c;
 /*reflex-sensor*/
 uint8_t reflex_previous = 0x00;
 uint8_t reflex_current = 0x00;
-uint8_t segments_turned = 0x00;
+uint16_t segments_turned = 0x00;
 uint8_t MR_Reflex = 0x00;
 
 /*GYRO*/
@@ -199,7 +199,7 @@ void transmitALL(){
 
 	transmitParallelR();
 	transmitParallelL();
-	transmitGyroT();
+	//transmitGyroT();
 	
 	transmitIRRFT();
 	transmitIRRBT();
