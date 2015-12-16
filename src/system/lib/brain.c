18cb0e70d40dd34e800ec81ab2f1a909b37b49c6
pettu298 <pettu298@student.liu.se>
2015-12-16 15:30:53 +0100

    commented and cleaned up brain.c

--- src/system/lib/brain.c
+++ src/system/lib/brain.c
@@ -1,7 +1,7 @@
 /*
  * Created: November 2015
  * Author : Mikael Å, Anton R, Michael S
- * "it should be working now" -Peter
+ * "It should be working now" -Peter
  *
  * The brain of the robot, using sensory data to make a desicion on which action to take now. It also makes sense of the map-data, using it to find 
  * islands to plunder. Yar har!
@@ -17,13 +17,11 @@ void find_empty_tile();
 uint8_t dfs(uint8_t startx, uint8_t starty);
 uint8_t dfs_help(uint8_t startx, uint8_t starty);
 void gen_actions();
-void find_target();
 void mark_walls();
 uint8_t done_unexp();
 uint8_t done_iwall();
 
-//
-uint8_t adj_matrix[32][32];
+
 uint8_t follow_wall = 1;
 uint8_t startup = 1;
 
@@ -32,14 +30,13 @@ uint8_t check = 0;
 uint8_t visited[32][32];
 uint8_t first_time_on_island = 0;
 
-
+/*
+ * Generate next action
+ */
 void think(){
-
-	debug = lets_go_home;
-
 	if((curr_action == EMPTY) && (map_complete == 0)){	
 
-		if(follow_wall == 1){ //Om vi ska följa högerväggen
+		if(follow_wall == 1){ //If in follow wall mode
 
 			if((s_ir_front < 11) && (s_ir_front > 2)){ //If to close to the wall, back up a bit
 					curr_action = BACKWARD;
@@ -48,11 +45,11 @@ void think(){
 			else if(((t_vagg_h_f == 0) && (t_vagg_h_b == 0)) || ((t_vagg_h_f == 1) && (t_vagg_h_b == 1))){ //If there is no wall to the right of the robot
 					curr_action = SPIN_R;
 				
-				if((s_ir_front > 12) && (s_ir_front < 30)){ 
+				if((s_ir_front > 12) && (s_ir_front < 30)){  //If we're close to wall, nudge to it.
 					curr_action = NUDGE_TO_WALL;
 				}
 
-				else if((t_vagg_v_f == 1) && (t_vagg_v_b == 1)){
+				else if((t_vagg_v_f == 1) && (t_vagg_v_b == 1)){	// parallelize against a wall far to the left
 					if(t_p_v != 0){
 						curr_action = P_WEAK_L;
 					}
@@ -74,10 +71,7 @@ void think(){
 						curr_action = NUDGE_TO_WALL;
 					}
 
-					else if((t_vagg_v_f == 0) || (t_vagg_v_f == 1)){ //Turn left
-						curr_action = SPIN_L;
-					}
-					else{ //If there is a wall both left and right, turn 180 deg
+					else{ //If there is a wall front and right, turn left
 						curr_action = SPIN_L;
 					}
 				}
@@ -86,13 +80,7 @@ void think(){
 				curr_action = FORWARD;
 			}
 
-			if(map_enclosed == 0){ //If we are following the outside wall
-				//	map_enclosed = 1;
-				//	mark_walls();
-				//	find_target();
-			//	}
-			}
-			else if(map_enclosed == 1){
+			if(map_enclosed == 1){	//outer wall is connected, enclosing the inner area.
 
 				uint8_t temp_x, temp_y;
 
@@ -120,7 +108,7 @@ void think(){
 
 				if((lets_go_home == 0) && (follow_island == 0)){
 					if ((rmem(robot_pos_x + temp_y, robot_pos_y - temp_x) == IWALL) || 
-						(rmem(robot_pos_x + temp_y * 2, robot_pos_y - temp_x * 2) == IWALL)){ //VÄNSTER IR WALL
+						(rmem(robot_pos_x + temp_y * 2, robot_pos_y - temp_x * 2) == IWALL)){ 
 							land_o_hoy = 0;
 							first_time_on_island = 1;
 							curr_action = PARALLELIZE;
@@ -139,36 +127,33 @@ void think(){
 
 void think_hard(){
 
-	if(map_enclosed == 0){ // dfs om vi har mer grejs att leta efter.
-		//if((robot_pos_x == home_x) && (robot_pos_y == home_y)){
+	if(map_enclosed == 0){ // Depth First Search to determine if the outer wall is closed
 		if(dfs(robot_pos_x, robot_pos_y) == 0){
-			//pstack(0xFF, 0xFF, 0xFF);		
-			enqueue(0xFF, 0xFF, 0xFF);	
+			enqueue(0xFF, 0xFF, 0xFF);		//Send special command to tell CRAY to rescale its map
 			map_enclosed = 1;
-			mark_walls();
+			mark_walls();					//Turns all squares outside of the enclosed area to walls
 		}
 	}
 
-	if((done_unexp() == 1) || (done_iwall() == 1)){
-		if(map_enclosed == 1){
-			lets_go_home = 1;
+	if((done_unexp() == 1) || (done_iwall() == 1)){		//If there are no unexplored tiles or no inner walls left
+		if(map_enclosed == 1){							//Is the area enclosed as well
+			lets_go_home = 1;							//We are finished mapping, let's return to start
 		}
 	}
 
-	if(follow_island == 1){
-		if((first_time_on_island == 0) || lets_go_home){
+	if(follow_island == 1){													
+		if((first_time_on_island == 0) || lets_go_home){					
 			if((robot_pos_x == island_x) && (robot_pos_y == island_y)){
-				land_o_hoy = 0;
+				land_o_hoy = 0;												
 				curr_action = PARALLELIZE;
 				next_action = SPIN_L;
 			}
 		}
 	}
 
-	if((lets_go_home == 1) && (robot_pos_x == home_x) && (robot_pos_y == home_y)){
-		//pstack(0xEE,0xEE,0xEE);
-		enqueue(0xEE, 0xEE, 0xEE);
-		setSpeed(0,0,FORWARD,FORWARD);
+	if((lets_go_home == 1) && (robot_pos_x == home_x) && (robot_pos_y == home_y)){		//Is system done?
+		enqueue(0xEE, 0xEE, 0xEE);														//Special command to tell CRAY that we're done
+		setSpeed(0,0,FORWARD,FORWARD);													//Stop motors
 		map_complete = 1;
 	}		
 }
@@ -181,153 +166,12 @@ struct tuple{
 		uint8_t y;
 };
 
-void find_path(){ //Kommer nog inte användas, man vill kunna stoppa in nudge osv.
-
-	uint8_t temp_x = target_x;
-	uint8_t temp_y = target_y;
-	uint8_t next_val = 255;
-	uint8_t next_dir = 0;
-	uint8_t old_dir = 0;
-	uint8_t old_x = target_x;
-	uint8_t old_y = target_y;
-	uint8_t next_x = 0;
-	uint8_t next_y = 0;
-
-	while((temp_x != robot_pos_x) && (temp_y != robot_pos_y)){ //While not at robot position
-
-			uint8_t curr_val = adj_matrix[temp_x][temp_y];
-
-			if((adj_matrix[temp_x-1][temp_y] > curr_val) && (temp_x-1 != old_x) && (temp_y != old_y)){ //North
-				adj_matrix[temp_x-1][temp_y] = 254;
-			}
-			else{
-				next_x = temp_x-1;
-				next_y = temp_y;
-			}
-
-			if((adj_matrix[temp_x][temp_y+1] > curr_val) && (temp_x != old_x) && (temp_y+1 != old_y)){ //East
-				adj_matrix[temp_x][temp_y+1] = 254;
-			}
-			else{
-				next_x = temp_x;
-				next_y = temp_y+1;
-			}
-
-			if((adj_matrix[temp_x+1][temp_y] > curr_val) && (temp_x+1 != old_x) && (temp_y != old_y)){ //South
-				adj_matrix[temp_x+1][temp_y] = 254;
-			}
-			else{
-				next_x = temp_x+1;
-				next_y = temp_y;
-			}
-
-			if((adj_matrix[temp_x][temp_y-1] > curr_val) && (temp_x != old_x) && (temp_y-1 != old_y)){ //West
-				adj_matrix[temp_x][temp_y-1] = 254;
-			}
-			else{
-				next_x = temp_x;
-				next_y = temp_y-1;
-			}
-	
-			
-		old_x = temp_x;
-		old_y = temp_y;
-
-		temp_x = next_x;
-		temp_y = next_y;
-	}
-}
 
 
-void find_target(){
-
-	tuple left_list[20];
-
-}
-
-void gen_adj_matrix(uint8_t home){
-
-	uint8_t i;
-	uint8_t j;
-	uint8_t list_front = 0;
-	tuple adj_list[99];
-
-
-	//1. Generate a list of tiles that are floor and unexplored tiles. 
-	//2. Iterate through the list and give each tile a number that is its lowest neighbour++
-	//3. When the list is empty, the coloring is done.
-
-	for(i = 0; i < 32; i++){
-		for(j = 0; j < 32; j++){
-
-			uint8_t temp = rmem(i,j);
-
-			if((temp == FLOOR) || (temp == UNEXP)){
-				tuple temptup = {.x = i, .y = j};
-				adj_list[list_front] = temptup;
-				list_front +=1;
-
-			}
-		}
-	}
-
-//----------Step 1 done------------------------
-
-	uint8_t target_found = 0;
-
-	while(!target_found){
-
-		uint8_t i;
-		for(i = 0; i < list_front; i++){
-			if(adj_list[i].x != 0){
-				uint8_t tempx = adj_list[i].x;
-				uint8_t tempy = adj_list[i].y;
-
-				uint8_t checkL = adj_matrix[tempx-1][tempy]; //Check left
-				uint8_t checkR = adj_matrix[tempx+1][tempy]; //Check right
-				uint8_t checkU = adj_matrix[tempx][tempy-1]; //Check up
-				uint8_t checkD = adj_matrix[tempx][tempy+1]; //Check down
-
-				uint8_t lowestN = checkL;
-
-				if((checkL != 254)||(checkR != 254)||(checkU != 254)||(checkD != 254)){
-
-					if((checkR != 254) && (checkR < lowestN)){
-						lowestN = checkR;
-					}
-					if((checkU != 254) && (checkU < lowestN)){
-						lowestN = checkU;
-					}
-					if((checkD != 254) && (checkD < lowestN)){
-						lowestN = checkR;
-					}
-					adj_matrix[tempx][tempy] = (lowestN +1);
-
-					if((rmem(tempx, tempy) == UNEXP) && !home){
-						target_x = tempx;
-						target_y = tempy;
-						target_found = 1;
-
-					}
-					else if(home && (tempx = home_x) && (tempy = home_y)){
-						
-						target_found = 1;
-
-					}
-
-
-					adj_list[i].x = 0;
-					adj_list[i].y = 0;
-				}
-			}
-		}
-	}
-
-return;
-
-}
-
-
+/*
+ * Returns 0 if there are any UNEXP tiles on the map.
+ * Otherwise returns 1.
+ */
 uint8_t done_unexp(){
 
 	uint8_t answer = 1;
@@ -344,6 +188,10 @@ uint8_t done_unexp(){
 	return answer;
 }
 
+/*
+ * Returns 0 if there are any IWALL tiles on the map.
+ * Otherwise returns 0.
+ */
 uint8_t done_iwall(){
 
 	uint8_t answer = 1;
@@ -360,7 +208,10 @@ uint8_t done_iwall(){
 	return answer;
 }
 
-
+/*
+ * Marks all unvisited squares as wall.
+ * This is used for marking all tiles outside of the enclosed area as wall.
+ */
 void mark_walls(){
 
 	uint8_t i;
@@ -375,7 +226,12 @@ void mark_walls(){
 	}
 }
 
-
+/*
+ * Depth First Search that tries to find a way out of the enclosed area to the outer line of OUTSIDE. 
+ * If this can be done, the area is not closed.
+ * Returns 1 if area is NOT closed.
+ * Returns 0 if area is closed.
+ */
 uint8_t dfs(uint8_t startx, uint8_t starty){
 	uint8_t i;
 	uint8_t j;
@@ -387,6 +243,11 @@ uint8_t dfs(uint8_t startx, uint8_t starty){
 	return dfs_help(startx, starty);
 }
 
+/*
+ * Recursive help function for DFS. 
+ * Returns 1 if area is NOT closed.
+ * Returns 0 if area is closed.
+ */
 uint8_t dfs_help(uint8_t startx, uint8_t starty){
 	if(visited[startx][starty] == 0){
 		if(rmem(startx,starty) == OUTSIDE){
